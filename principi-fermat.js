4a8d98f62f554e0ec40f024b6daf711c1777f597
martaw-code <marta.granero.i@estudiantat.upc.edu>
2023-05-23 08:55:50 +0200

    exemple simple

---
+++ principi-fermat.js
@@ -0,0 +1,10 @@
+function setup() {
+    var canvas = createCanvas(800, 400);
+    canvas.parent("myCanvas");
+}
+
+
+function draw() {
+    background(255);
+    ellipse(width / 2, height / 2, 100, 100);
+}
