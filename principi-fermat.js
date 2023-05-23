b393ce1bb3e5c113fc7896b42418eb91d81bc008
martaw-code <marta.granero.i@estudiantat.upc.edu>
2023-05-23 12:27:00 +0200

    holi

--- principi-fermat.js
+++ principi-fermat.js
@@ -2,7 +2,8 @@
 let N = 10; // Nombre de regions
 let n1, n2; // Índexs de refracció dels medis
 let v1, v2; // Velocitat propagació de la llum en el dos medis
-let delta = 0.1; // delta := valor màxim del canvi random en y(i)
+let delta = 50; // delta := valor màxim del canvi random en y(i)
+let c = 1; //Velocitat de la llum
 
 let y = []; // Coordenades aleatòries y de la trajectòria
 
@@ -11,8 +12,8 @@ let y = []; // Coordenades aleatòries y de la trajectòria
 function setParameters(index1, index2) {
     n1 = index1; // índex refracció primer medi
     n2 = index2; // Índex de refracció segon medi
-    v1 = 1 / n1; // Velocitat propagació de la llum en el primer medi
-    v2 = 1 / n2; // Velocitat propagació de la llum en el segon medi
+    v1 = c / n1; // Velocitat propagació de la llum en el primer medi
+    v2 = c / n2; // Velocitat propagació de la llum en el segon medi
 }
 
 function apartat_a() {
@@ -27,9 +28,11 @@ function color_linies(red, green, blue) {
     stroke(red, green, blue)
 }
 
+
 function amplada_linies(amplada_pixels) {
     strokeWeight(amplada_pixels);
 }
+
 
 function draw_canvas_vora() { 
     amplada_linies(3);
@@ -38,22 +41,45 @@ function draw_canvas_vora() {
     rect(0, 0, width, height);
 }
 
+
+function print_y_valors() {
+    let yValuesDiv = document.getElementById("yValues");
+    yValuesDiv.innerHTML = "valors de y: " + y.join(", ");
+}
+
+
+//Inicialitzem les coordenades y aleatòriament
+function setup_random_y_trajectories() {
+    for (let i = 0; i <= N; i++) {
+        y[i] = random(height); // fem servir com alçada la mida del canvas
+    }
+    print_y_valors();
+}
+
+
+function update_regions(value) {
+    N = value; // actualitzem valor N
+    setup_random_y_trajectories(); // Actualitzem el valor de les trajectories random basades en la nova N
+    draw_regions(); // redibuixem les regions
+}
+
+
+function crea_N_slider() {
+    let slider = createSlider(1, 20, N);
+    slider.parent("container-slider"); // posem el sliders en el contenidor HTML
+    slider.input(() => update_regions(slider.value())); // actualitzem el valor de regions quan el valor del slider canvia
+}
+
 //Dibuixem les N regions uniformes on calculem la coordenada x de cada regió en funció del seu índex
 function draw_regions() {
     color_linies(0,0,0);
     amplada_linies(2);
 
     for (let i = 1; i < N; i++) {
-        //map(valor a convertir, limit inf rang actual, limit sup rang actual, limit inf rang desitjat, limit sup rang desitjat)
+        //map(valor a convertir, limit inf rang actual, limit sup rang actual, 
+        //limit inf rang desitjat, limit sup rang desitjat)
         let x = map(i, 0, N, 0, width); //calculem on hem de partir les regions en base a la mida del canvas
         line(x, 0, x, height); //line: traça linia entre dos punts line(x1,y1,x2,y2)
-    }
-}
-
-//Inicialitzem les coordenades y aleatòriament
-function setup_random_y_trajectories() {
-    for (let i = 0; i <= N; i++) {
-        y[i] = random(height); // fem servir com alçada la mida del canvas
     }
 }
 
@@ -69,12 +95,60 @@ function trajectoria_llum() {
     }
 }
 
+
+// Cal minimitzar aquesta funció que calcula el temps de propagació de la llum
+function calcular_temps_propagacio_llum() {
+    let t = 0;
+    for (let i = 0; i < N; i++) {
+      let dx = dist(i, y[i], i + 1, y[i + 1]); //distancia euclidea entre regió i-i+1
+      let v = (i < N-1) ? v1 : v2; //tots els índexs són uniformes i son n1, menys el darrer que és n2
+      t += dx / v;
+    }
+    return t;
+}
+
+
+//Fem un canvi random en les y en una regio i acualitzem el temps de propagació
+/*Es considera una i a l’atzar (0<i<N) i es fa un canvi aleatori en el valor de y(i) entre -delta i +delta. */
+//Calculeu si aquest canvi fa disminuir el temps que triga la llum. 
+function random_canvi_y() {
+    let y_noves = y.slice(); // copia l'array de les y en un nou array coordenades actuals
+    let i = floor(random(1, N)); // escollim una regió aleatoriament
+    
+    let dy = random(-delta, delta); // calculem un canvi aleatori en y(i)
+    y_noves[i] += dy;
+
+    let t_update = 0;
+    for (let j = 0; j < N; j++) {
+        let dx = dist(j, y_noves[j], j + 1, y_noves[j + 1]);
+        let v = (j < N-1) ? v1 : v2;
+        t_update += dx / v;
+    }
+    return [t_update,y_noves];
+}
+
+
+//Calculeu si aquest canvi fa disminuir el temps que triga la llum. 
+//En cas afirmatiu, accepteu el canvi i continueu el procés.*/
+function calcular_trajectoria() { 
+    let t_prop = calcular_temps_propagacio_llum();
+    let t_prop_nou = random_canvi_y()[0];
+    let y_noves = random_canvi_y()[1];
+
+    //comprobem si el nou canvi disminueix el temps de propagació
+    if (t_prop_nou < t_prop) {
+        y = y_noves.slice(); // acceptem el canvi
+    }
+}
+
+
 function setup() {
     var canvas = createCanvas(800, 400);
     canvas.parent("myCanvas");
 
     apartat_a();
     setup_random_y_trajectories();
+    crea_N_slider();
 }
 
 
@@ -84,4 +158,6 @@ function draw() {
     draw_canvas_vora();
     draw_regions();
     trajectoria_llum();
+
+    calcular_trajectoria();
 }
